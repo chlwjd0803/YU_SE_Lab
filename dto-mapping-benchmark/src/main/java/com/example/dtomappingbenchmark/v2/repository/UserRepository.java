f6c8f17a6aeec083273bfb87389d4d0a24917a03
JUNG CHOI <160298290+chlwjd0803@users.noreply.github.com>
2025-08-03 00:28:51 +0900

    Revert "[Mapping-Benchmark] Fetch EAGER 강제, 엔티티 그래프 제거"
    This reverts commit ead2c9824a28abdcadda895098c6721dca3faf3f.

--- dto-mapping-benchmark/src/main/java/com/example/dtomappingbenchmark/v2/repository/UserRepository.java
+++ dto-mapping-benchmark/src/main/java/com/example/dtomappingbenchmark/v2/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.dtomappingbenchmark.v2.repository;
 
 import com.example.dtomappingbenchmark.v2.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository("userRepositoryV2")
 public interface UserRepository extends JpaRepository <User, Long>{
+    @EntityGraph(attributePaths = "posts") // ← 여기가 핵심
     Optional<User> findById(Long id);
 }
